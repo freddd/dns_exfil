f92589db3806884f87312c384a7d37d46f96b576
Fredrik Grönqvist <fredrik.gronqvist@embark-studios.com>
2021-02-26 16:29:36 +0100

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,22 @@ fn main() -> std::io::Result<()> {
     let bind_addr = "0.0.0.0:53";
 
     let socket = UdpSocket::bind(bind_addr)?;
- 
+
     loop {
         let mut buf = [0; 2048];
         let (_amt, _src) = socket.recv_from(&mut buf)?;
         match Packet::parse(&buf) {
-            Ok(p) => {
-                p.questions
-                .into_iter()
-                .for_each(|q|
-                    println!("{}", q.qname
+            Ok(p) => p.questions.into_iter().for_each(|q| {
+                println!(
+                    "{}",
+                    q.qname
                         .to_string()
                         .split('.')
                         .collect::<Vec<&str>>()
                         .first()
                         .unwrap()
-                ))            
-            }
+                )
+            }),
             Err(e) => {
                 println!("error parsing packet: {:#?}", e)
             }
